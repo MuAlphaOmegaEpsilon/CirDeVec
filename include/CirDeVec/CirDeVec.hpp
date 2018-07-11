2e67ec3d7e6e6bf24c56fa1cdcd2e4b868192503
Tommaso Bonvicini <tommasobonvicini@gmail.com>
2018-07-11 19:39:12 +0200

    A baseline for the code to add later on

---
+++ include/CirDeVec/CirDeVec.hpp
@@ -0,0 +1,20 @@
+/**
+ * @brief This is the header file for the C++ implementation of CirDeVec, a Circular Double-Ended Vector data-structure. 
+ * CirDeVec contains a templated implementation of a mixed data-structure that is heavily inspired by the behaviour of
+ * circular buffers, double-ended queues, and vectors.
+ * CirDeVec, also called CDV for short, basically behaves as a symmetric vector, where symmetrical means that doing an insert/remove operation near
+ * the head has equal cost compared to the same operation near the head, instead of the classical asymmetrical vector, where asymmetrical means
+ * that the cost of doing an insert/remove operation near the head has a much higher cost compared to the same operation near the tail.
+ * @file CirDeVec.hpp
+ * @author Tommaso Bonvicini <tommasobonvicini@gmail.com> https://github.com/MuAlphaOmegaEpsilon/MinLog
+ * @date 11-07-2018
+ */
+
+// Never include this header twice or more in a row
+#ifndef CIRDEVEC_HPP 
+#define CIRDEVEC_HPP
+#pragma once
+
+
+
+#endif
