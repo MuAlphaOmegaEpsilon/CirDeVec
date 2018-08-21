651b94105a480c064ea0f204128e4e2807bb5c7f
Tommaso Bonvicini <tommasobonvicini@gmail.com>
2018-08-21 20:07:55 +0200

    Update Google Benchmark to the latest version

--- scripts/downloadTestDependencies.sh
+++ scripts/downloadTestDependencies.sh
@@ -1,5 +1,5 @@
 cd ..
-git submodule update --init
+git submodule update --recursive --init
 cd ./include/benchmark
 mkdir build && cd build
 cmake .. -DCMAKE_BUILD_TYPE=RELEASE -DBENCHMARK_DOWNLOAD_DEPENDENCIES=ON -DBENCHMARK_ENABLE_GTEST_TESTS=OFF -DBENCHMARK_ENABLE_INSTALL=OFF
