16499a7706dc57adc51e57576c2709e401b8bb07
Tommaso Bonvicini <tommasobonvicini@gmail.com>
2018-08-26 18:55:38 +0200

    Little edits to support the new placement of the Google Benchmark library

--- scripts/downloadTestDependencies.sh
+++ scripts/downloadTestDependencies.sh
@@ -1,6 +1,6 @@
 cd ..
-git submodule update --recursive --init
-cd ./include/benchmark
+git submodule update --init
+cd ./extra/benchmark
 mkdir build && cd build
 cmake .. -DCMAKE_BUILD_TYPE=RELEASE -DBENCHMARK_DOWNLOAD_DEPENDENCIES=ON -DBENCHMARK_ENABLE_GTEST_TESTS=OFF -DBENCHMARK_ENABLE_INSTALL=OFF
 make
