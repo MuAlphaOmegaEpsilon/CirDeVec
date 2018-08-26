83476be662de46b48d54041057fcee040b322c1c
Tommaso Bonvicini <tommasobonvicini@gmail.com>
2018-08-26 19:36:35 +0200

    The scripts for downloading the Google Benchmark library dependencies are no longer needed, since everything is done my CMake now

--- scripts/downloadTestDependencies.sh
+++
@@ -1,6 +0,0 @@
-cd ..
-git submodule update --init
-cd ./extra/benchmark
-mkdir build && cd build
-cmake .. -DCMAKE_BUILD_TYPE=RELEASE -DBENCHMARK_DOWNLOAD_DEPENDENCIES=ON -DBENCHMARK_ENABLE_GTEST_TESTS=OFF -DBENCHMARK_ENABLE_INSTALL=OFF
-make
