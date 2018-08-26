4fe1dfbff773cd949daa25d4b30c8a7807541e9f
Tommaso Bonvicini <tommasobonvicini@gmail.com>
2018-08-26 18:48:33 +0200

    Added bench and test building support in a separate cmake folder

---
+++ cmake/buildBenchmark.cmake
@@ -0,0 +1,21 @@
+IF (CMAKE_SOURCE_DIR STREQUAL CMAKE_CURRENT_SOURCE_DIR)						# This is true only when someone is building the CirDeVec standalone
+
+	SET (GBENCH "extra/benchmark")											# A simple shortcut for the path "extra/benchmark"
+
+	SET (CMAKE_BUILD_TYPE RELEASE)
+	OPTION (BENCHMARK_ENABLE_TESTING "Enable testing of the benchmark library." OFF)	
+	OPTION (BENCHMARK_DOWNLOAD_DEPENDENCIES "Allow the downloading and in-tree building of unmet dependencies" ON)
+	OPTION (BENCHMARK_ENABLE_INSTALL "Enable installation of benchmark. (Projects embedding benchmark may want to turn this OFF.)" OFF)
+	OPTION (BENCHMARK_ENABLE_GTEST_TESTS "Enable building the unit tests which depend on gtest" OFF)
+	ADD_SUBDIRECTORY (${GBENCH})											# Adding the Google Benchmark library's CMakeLists.txt file
+			
+	ADD_EXECUTABLE (bench "tests/bench.cpp")							# Add the benchmark.cpp as an executable to build
+
+	TARGET_INCLUDE_DIRECTORIES (bench PUBLIC extra/benchmark/include)		# The include folder of the Google Benchmark library
+
+	FIND_PACKAGE (Threads REQUIRED)											# Finding pthread, since it is REQUIRED
+	TARGET_LINK_LIBRARIES (bench pthread)									# It is necessary to link with the pthread library due to how GCC implements std::thread
+	TARGET_LINK_LIBRARIES (bench libbenchmark.a)							# Linking to the Google Benchmark static library
+	TARGET_LINK_LIBRARIES (bench -L/extra/benchmark/build/src/)				# Linking to the Google Benchmark static library
+	
+ENDIF ()
---
+++ cmake/buildTests.cmake
@@ -0,0 +1,8 @@
+IF (CMAKE_SOURCE_DIR STREQUAL CMAKE_CURRENT_SOURCE_DIR)
+	
+	ENABLE_TESTING ()														# Enable the testing generation
+
+	#ADD_EXECUTABLE (push_back "tests/push_back.cpp")						# Add the push_back.cpp as an executable to build
+	#ADD_TEST (PUSH_BACK push_back)											# Add the test PUSH_BACK to run the push_back executable
+
+	ENDIF ()
