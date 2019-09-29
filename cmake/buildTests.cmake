eeffbe91ba54b0604d3866ae05adf937c2f43ef0
MuAlphaOmegaEpsilon <tommasobonvicini@gmail.com>
2019-09-29 13:08:27 +0200

    Update buildTests.cmake file

--- cmake/buildTests.cmake
+++ cmake/buildTests.cmake
@@ -4,6 +4,7 @@ IF (CMAKE_SOURCE_DIR STREQUAL CMAKE_CURRENT_SOURCE_DIR)
 
 	# EXAMPLE WITH A push_back SOURCE FILE:
 	#ADD_EXECUTABLE (push_back "tests/push_back.cpp")						# Add the push_back.cpp as an executable to build
+	#TARGET_LINK_LIBRARIES (push_back CirDeVec)								# Linking to the CirDeVec static library
 	#ADD_TEST (PUSH_BACK push_back)											# Add the test PUSH_BACK to run the push_back executable
 
-	ENDIF ()
+ENDIF ()
